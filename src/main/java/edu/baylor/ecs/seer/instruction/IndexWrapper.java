d0ed4f5905a79071c90ef256684ec357af1f9a7b
Dipta Das <dipta670@gmail.com>
2019-10-14 00:35:04 -0500

    Modified JavaAssist InstructionPrinter to InstructionScanner

---
+++ src/main/java/edu/baylor/ecs/seer/instruction/IndexWrapper.java
@@ -0,0 +1,16 @@
+package edu.baylor.ecs.seer.instruction;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class IndexWrapper {
+    private int index;
+    private String type;
+    private Object instruction;
+}
