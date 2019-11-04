bc21b4e9c3748c667fdb257a0afe72a7e3670458
Dipta Das <dipta670@gmail.com>
2019-11-04 16:19:39 -0600

    Add AllArgsConstructor

--- library/src/main/java/edu/baylor/ecs/cloudhubs/rad/context/RadRequestContext.java
+++ library/src/main/java/edu/baylor/ecs/cloudhubs/rad/context/RadRequestContext.java
@@ -1,10 +1,14 @@
 package edu.baylor.ecs.cloudhubs.rad.context;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
+@AllArgsConstructor
 public class RadRequestContext {
     private String pathToCompiledMicroservices;
     private String organizationPath;
