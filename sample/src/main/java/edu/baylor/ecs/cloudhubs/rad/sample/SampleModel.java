abe87b4e3448f53191678db295224e23941bb89c
Dipta Das <dipta670@gmail.com>
2019-12-29 03:56:55 -0600

    Update SampleModel.java

--- sample/src/main/java/edu/baylor/ecs/cloudhubs/rad/sample/SampleModel.java
+++ sample/src/main/java/edu/baylor/ecs/cloudhubs/rad/sample/SampleModel.java
@@ -1,5 +1,14 @@
 package edu.baylor.ecs.cloudhubs.rad.sample;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SampleModel {
     Long id;
     String name;
