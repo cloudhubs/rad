d980fa044881cf5aae3ce3a46cdbc48cc24d8213
Dipta Das <dipta670@gmail.com>
2019-12-29 04:41:19 -0600

    Extract entrypoint from docker inspect and jq

--- cli/extract.sh
+++ cli/extract.sh
@@ -1,7 +1,11 @@
+set -x
+
 mkdir target
 
-docker create -ti --name dummy ${TARGET_IMAGE} bash
-docker cp dummy:/rad-sample.jar target/rad-sample.jar
+TARGET_JAR=$(docker inspect ${TARGET_IMAGE} | jq -r '.[0].Config.Entrypoint[-1]')
+
+docker create -ti --name dummy diptadas/rad-sample bash
+docker cp dummy:${TARGET_JAR} target/target.jar
 docker rm -f dummy
 
 java -jar rad-cli.jar
