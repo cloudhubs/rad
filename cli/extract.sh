e3cd4373f647f83be74070a7b071942e8cded47b
Dipta Das <dipta670@gmail.com>
2020-01-07 00:21:31 -0600

    Target image name from env var

--- cli/extract.sh
+++ cli/extract.sh
@@ -4,7 +4,7 @@ mkdir target
 
 TARGET_JAR=$(docker inspect ${TARGET_IMAGE} | jq -r '.[0].Config.Entrypoint[-1]')
 
-docker create -ti --name dummy diptadas/rad-sample bash
+docker create -ti --name dummy ${TARGET_IMAGE} bash
 docker cp dummy:${TARGET_JAR} target/target.jar
 docker rm -f dummy
 
